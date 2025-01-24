8f6eaf37ad74a05607e023681015a9ee43778053
Alessandro N Sousa <allessandro188@gmail.com>
2025-01-23 21:14:13 -0300

    novos modelos de components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TableComponent } from './components/table/table.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path:'card',component:CardComponent,
+  },
+  {
+    path:'slider',component:SliderComponent,
+  },
+  {
+    path:'table',component:TableComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
 import { MaterialModule } from './material-modules';
+import { CardComponent } from './components/card/card.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TableComponent } from './components/table/table.component';
 
 
 @NgModule({
@@ -21,6 +21,9 @@ import { MaterialModule } from './material-modules';
     HomeComponent,
     NavbarComponent,
     SidebarComponent,
+    CardComponent,
+    SliderComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +31,7 @@ import { MaterialModule } from './material-modules';
     BrowserAnimationsModule,
     MatToolbarModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
